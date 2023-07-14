0edc90fa832775f1cf339f675c426fb64843c1d7
abdo-355 <abdoluxor366@gmail.com>
2023-07-14 04:35:50 +0300

    start on the binary search tree and add creation, insertion and lookup. with their tests

--- src/__test__/binary-search-tree.test.ts
+++ src/__test__/binary-search-tree.test.ts
@@ -1,9 +1,10 @@
-import { BinarySearchTreeNode } from "../binary-search-tree";
+import { BinarySearchTreeNode, BinarySearchTree } from "../binary-search-tree";
 
 describe("BinarySearchTreeNode", () => {
   it("should create a correct binary search tree node", () => {
     const node = new BinarySearchTreeNode(1);
     expect(node.value).toBe(1);
+    expect(node.frequency).toBe(1);
     expect(node.left).toBeNull();
     expect(node.right).toBeNull();
   });
@@ -11,6 +12,7 @@ describe("BinarySearchTreeNode", () => {
   it("should accept string as value", () => {
     const node = new BinarySearchTreeNode("test");
     expect(node.value).toBe("test");
+    expect(node.frequency).toBe(1);
     expect(node.left).toBeNull();
     expect(node.right).toBeNull();
   });
@@ -18,6 +20,7 @@ describe("BinarySearchTreeNode", () => {
   it("should accept number as value", () => {
     const node = new BinarySearchTreeNode(1);
     expect(node.value).toBe(1);
+    expect(node.frequency).toBe(1);
     expect(node.left).toBeNull();
     expect(node.right).toBeNull();
   });
@@ -25,6 +28,7 @@ describe("BinarySearchTreeNode", () => {
   it("should accept Date as value", () => {
     const node = new BinarySearchTreeNode(new Date());
     expect(node.value).toBeInstanceOf(Date);
+    expect(node.frequency).toBe(1);
     expect(node.left).toBeNull();
     expect(node.right).toBeNull();
   });
@@ -36,5 +40,218 @@ describe("BinarySearchTreeNode", () => {
     }).toThrowError(
       "type of Binary search tree node value must be string, number or Date"
     );
+  });
+});
+
+describe("BinarySearchTree", () => {
+  describe("creation", () => {
+    it("should create an empty binary search tree if not given any value", () => {
+      const tree = new BinarySearchTree();
+      expect(tree.root).toBeNull();
+      expect(tree.dataType).toBeNull();
+    });
+
+    it("should create a correct binary search tree with a given data type", () => {
+      const tree = new BinarySearchTree("test");
+      const expectedRoot = new BinarySearchTreeNode("test");
+
+      // JSON.stringify is used to compare the object because the object's reference is different
+      expect(JSON.stringify(tree.root)).toEqual(JSON.stringify(expectedRoot));
+      expect(tree.dataType).toBe("string");
+    });
+
+    it("should have a number dateType if given a number", () => {
+      const tree = new BinarySearchTree(1);
+      expect(tree.dataType).toBe("number");
+    });
+
+    it("should have a date dateType if given a date", () => {
+      const tree = new BinarySearchTree(new Date());
+      expect(tree.dataType).toBe("date");
+    });
+  });
+
+  describe("insertion", () => {
+    it("should insert to the root if the tree is empty", () => {
+      const tree = new BinarySearchTree();
+      expect(tree.root).toBeNull();
+
+      tree.insert(1);
+      expect(tree.root).toBeDefined();
+      expect(tree.root?.value).toBe(1);
+    });
+
+    it("should insert in descending order if the value is lower than the previous node", () => {
+      const tree = new BinarySearchTree(5);
+      tree.insert(3);
+      tree.insert(2);
+
+      /*
+              5
+            3
+          2
+      */
+      expect(tree.root?.value).toBe(5);
+      expect(tree.root?.left?.value).toBe(3);
+      expect(tree.root?.left?.left?.value).toBe(2);
+    });
+
+    it("should insert in ascending order if the value is higher than the previous node", () => {
+      const tree = new BinarySearchTree(5);
+      tree.insert(7);
+      tree.insert(8);
+
+      /*
+              5
+                 7
+                    8
+      */
+      expect(tree.root?.value).toBe(5);
+      expect(tree.root?.right?.value).toBe(7);
+      expect(tree.root?.right?.right?.value).toBe(8);
+    });
+
+    it("should insert to the correct position", () => {
+      const tree = new BinarySearchTree(5);
+      tree
+        .insert(3)
+        .insert(1)
+        .insert(2)
+        .insert(4)
+        .insert(6)
+        .insert(9)
+        .insert(7)
+        .insert(10)
+        .insert(8);
+
+      /*
+         5
+       /   \
+      3     6
+     / \     \
+    1   4     9
+     \       / \
+      2     7   10
+             \
+              8
+      */
+
+      // Validate the structure of the tree
+      expect(tree?.root!.value).toBe(5);
+
+      // Left subtree
+      expect(tree.root!.left!.value).toBe(3);
+      expect(tree.root!.left!.left!.value).toBe(1);
+      expect(tree.root!.left!.left!.right!.value).toBe(2);
+      expect(tree.root!.left!.right!.value).toBe(4);
+
+      // Right subtree
+      expect(tree.root!.right!.value).toBe(6);
+      expect(tree.root!.right!.right!.value).toBe(9);
+      expect(tree.root!.right!.right!.left!.value).toBe(7);
+      expect(tree.root!.right!.right!.right!.value).toBe(10);
+      expect(tree.root!.right!.right!.left!.right!.value).toBe(8);
+    });
+
+    it("should increase the frequency of the node if it already exists", () => {
+      const tree = new BinarySearchTree(5);
+      expect(tree.root?.frequency).toBe(1);
+
+      tree.insert(5);
+
+      expect(tree.root?.value).toBe(5);
+      expect(tree.root?.frequency).toBe(2);
+
+      const tree2 = new BinarySearchTree(5);
+
+      tree2.insert(3);
+      tree2.insert(1);
+      tree2.insert(3);
+
+      expect(tree2.root?.left?.frequency).toBe(2);
+    });
+
+    it("should support method chaining", () => {
+      const tree = new BinarySearchTree(5);
+
+      tree.insert(5).insert(3).insert(1).insert(3);
+
+      expect(tree.root?.left?.frequency).toBe(2);
+      expect(tree.root?.left?.left?.value).toBe(1);
+    });
+
+    it("should throw an error if we insert unsupported data types", () => {
+      const tree = new BinarySearchTree();
+      expect(() => {
+        //@ts-ignore
+        tree.insert([5]);
+      }).toThrowError(
+        "type of Binary search tree node value must be string, number or Date"
+      );
+    });
+
+    it("should throw an error if the value doesn't match the tree's data type", () => {
+      const tree = new BinarySearchTree(5);
+      expect(() => {
+        tree.insert("test");
+      }).toThrowError("tree of type number doesn't support string value");
+      expect(() => {
+        tree.insert(new Date());
+      }).toThrowError("tree of type number doesn't support date value");
+
+      const tree2 = new BinarySearchTree("test");
+      expect(() => {
+        tree2.insert(1);
+      }).toThrowError("tree of type string doesn't support number value");
+
+      const tree3 = new BinarySearchTree(new Date());
+      expect(() => {
+        tree3.insert(1);
+      }).toThrowError("tree of type date doesn't support number value");
+    });
+  });
+
+  describe("lookup", () => {
+    it("should return null if the value doesn't exist", () => {
+      const tree = new BinarySearchTree(5);
+      expect(tree.lookup(6)).toBeNull();
+      expect(tree.lookup(5)).not.toBeNull();
+    });
+
+    it("should return the node with the given value", () => {
+      const tree = new BinarySearchTree(5);
+      tree
+        .insert(3)
+        .insert(1)
+        .insert(2)
+        .insert(4)
+        .insert(6)
+        .insert(9)
+        .insert(7)
+        .insert(10)
+        .insert(8);
+
+      // the root
+      expect(tree.lookup(5)).not.toBeNull();
+      expect(tree.lookup(5)).toEqual(tree.root);
+
+      // the smallest value
+      expect(tree.lookup(1)).not.toBeNull();
+      expect(tree.lookup(1)).toEqual(tree.root!.left!.left);
+
+      // the biggest value
+      expect(tree.lookup(10)).not.toBeNull();
+      expect(tree.lookup(10)).toEqual(tree.root!.right!.right!.right);
+
+      // random value
+      expect(tree.lookup(8)).not.toBeNull();
+      expect(tree.lookup(8)).toEqual(tree.root!.right!.right!.left!.right);
+    });
+  });
+
+  it("should work on a tree with one node", () => {
+    const tree = new BinarySearchTree(5);
+    expect(tree.lookup(5)).not.toBeNull();
+    expect(tree.lookup(5)).toEqual(tree.root);
   });
 });
--- src/binary-search-tree.ts
+++ src/binary-search-tree.ts
@@ -3,6 +3,7 @@ type BSTVNodeValue = string | number | Date;
 export class BinarySearchTreeNode {
   // the value should be only these so that we can compare them with > and <
   public value: BSTVNodeValue;
+  public frequency: number;
   public right: BinarySearchTreeNode | null;
   public left: BinarySearchTreeNode | null;
 
@@ -13,6 +14,7 @@ export class BinarySearchTreeNode {
       );
     }
     this.value = value;
+    this.frequency = 1;
     this.right = null;
     this.left = null;
   }
@@ -24,4 +26,89 @@ export class BinarySearchTreeNode {
       value instanceof Date
     );
   };
+}
+
+export class BinarySearchTree {
+  public root: BinarySearchTreeNode | null;
+  public dataType: string | null;
+
+  constructor(value?: BSTVNodeValue) {
+    // we don't create a node for the root if not given a value
+    if (!value) {
+      this.root = null;
+      this.dataType = null;
+    } else {
+      this.root = new BinarySearchTreeNode(value);
+      this.dataType = this.getType(value);
+    }
+  }
+
+  private getType = (value: any) => {
+    /*
+     * if it's a date. 'typeof value' will return a 'object' value
+     * we fix it by doing this
+     */
+    return value instanceof Date ? "date" : typeof value;
+  };
+
+  public lookup = (value: BSTVNodeValue) => {
+    let curr = this.root;
+
+    while (curr) {
+      if (curr.value === value) return curr;
+
+      if (value > curr.value) {
+        curr = curr.right;
+      } else {
+        curr = curr.left;
+      }
+    }
+
+    return null;
+  };
+
+  public insert(value: BSTVNodeValue) {
+    const newNode = new BinarySearchTreeNode(value);
+
+    // if there is no root, we set the new node as the root and we set the data type
+    if (!this.root) {
+      this.root = newNode;
+      // view the constructor to understand this line
+      this.dataType = this.getType(value);
+      return this;
+    }
+
+    // if the data type doesn't match we throw an error
+    if (this.getType(value) !== this.dataType) {
+      throw new Error(
+        `tree of type ${this.dataType} doesn't support ${this.getType(
+          value
+        )} value`
+      );
+    }
+
+    let curr: BinarySearchTreeNode | null = this.root;
+    let prev: BinarySearchTreeNode;
+
+    while (curr) {
+      if (value === curr.value) {
+        curr.frequency++;
+        return this;
+      } else if (value > curr.value) {
+        prev = curr;
+        curr = curr.right;
+      } else {
+        prev = curr;
+        curr = curr.left;
+      }
+    }
+
+    if (value > prev!.value) {
+      prev!.right = newNode;
+    } else {
+      prev!.left = newNode;
+    }
+
+    return this;
+  }
 }
